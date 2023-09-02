40726988394bffeb526acef8f37d865a0dfc09ca
Papuna Fshaveli <papunafshaveli@gmail.com>
2023-09-03 02:21:19 +0400

    add html and css files

---
+++ app.js
@@ -0,0 +1,4 @@
+let lengthInput = document.getElementById("password-length-range");
+let lengthNumber = document.querySelector(".length-number");
+console.log((lengthNumber.textContent = lengthInput.value));
+console.log(lengthNumber);
